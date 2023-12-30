9c999cc10530d77ffee563401a720aec4d7be518
BugsFounder <bugsfounder2021@gmail.com>
2023-12-30 10:29:41 +0530

    Baisc console hello app and gui app

---
+++ hello/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug()<<"hello world";
+
+    return a.exec();
+}
