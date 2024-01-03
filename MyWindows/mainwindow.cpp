e3785fcd7755d42d1a69b2f317f5689bce28618c
BugsFounder <bugsfounder2021@gmail.com>
2024-01-03 08:31:53 +0530

    display windows in qt using dialog

---
+++ MyWindows/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setCentralWidget (ui->plainTextEdit);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionNew_Window_triggered()
+{
+
+}
+
