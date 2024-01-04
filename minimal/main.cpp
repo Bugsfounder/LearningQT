a37fb33f67c2d9b903dae9993ae2f064f52585d6
BugsFounder <bugsfounder2021@gmail.com>
2024-01-04 14:55:17 +0530

    Horizontal and Vertical Layouts QWidget *window = new QWidget(); window->setWindowTitle("My App");
      // creating buttons
        QPushButton *button1 = new QPushButton("one");
        QPushButton *button2 = new QPushButton("two");
        QPushButton *button3 = new QPushButton("three");
    
        // layouts
        // QHBoxLayout *hlayout = new QHBoxLayout();
        QVBoxLayout *vlayout = new QVBoxLayout();
    
        // Horizontal Layout
        // hlayout->addWidget(button1);
        // hlayout->addWidget(button2);
        // hlayout->addWidget(button3);
    
        // Vertical Layout
        vlayout->addWidget(button1);
        vlayout->addWidget(button2);
        vlayout->addWidget(button3);
    
        // window->setLayout(hlayout);
        window->setLayout(vlayout);
    
        window->show();

--- minimal/main.cpp
+++ minimal/main.cpp
@@ -1,11 +1,37 @@
 #include "QApplication"
-#include "QLabel"
-
+#include "QPushButton"
+#include "QHBoxLayout"
+#include "QVBoxLayout"
 
 int main(int agrc, char* agrv[]){
     QApplication app(agrc, agrv);
-    QLabel* label = new QLabel("<h2>Hello <font color=red><i>World</i></font> </h2>");
-    label->show();
 
+    QWidget *window = new QWidget();
+    window->setWindowTitle("My App");
+
+    // creating buttons
+    QPushButton *button1 = new QPushButton("one");
+    QPushButton *button2 = new QPushButton("two");
+    QPushButton *button3 = new QPushButton("three");
+
+    // layouts
+    // QHBoxLayout *hlayout = new QHBoxLayout();
+    QVBoxLayout *vlayout = new QVBoxLayout();
+
+    // Horizontal Layout
+    // hlayout->addWidget(button1);
+    // hlayout->addWidget(button2);
+    // hlayout->addWidget(button3);
+
+    // Vertical Layout
+    vlayout->addWidget(button1);
+    vlayout->addWidget(button2);
+    vlayout->addWidget(button3);
+
+
+    // window->setLayout(hlayout);
+    window->setLayout(vlayout);
+
+    window->show();
     return app.exec();
 }
