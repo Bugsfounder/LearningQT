ce60e068f6419dd7019a87527f3e3402274f9b60
BugsFounder <bugsfounder2021@gmail.com>
2024-01-13 11:42:29 +0530

    QListViewModel Model view, editable list, adding, inserting, deleting data from list Dialog::Dialog(QWidget *parent)     : QDialog(parent)     , ui(new Ui::Dialog) {     ui->setupUi(this);     model = new QStringListModel(this);
        QStringList List;
        List << "Cat" << "Dog" << "Rabbit";
    
        model->setStringList(List);
    
        ui->listView->setModel(model);
        ui->comboBox->setModel(model);
    
        ui->listView->setEditTriggers(QAbstractItemView::AnyKeyPressed | QAbstractItemView::DoubleClicked);
    }
    
    Dialog::~Dialog()
    {
        delete ui;
    }
    
    void Dialog::on_pushButton_clicked()
    {
        // add
        int row = model->rowCount();
        model->insertRow(row);
    
        QModelIndex  index = model->index(row);
    
        ui->listView->setCurrentIndex(index);
        ui->listView->edit(index);
    }
    
    void Dialog::on_pushButton_2_clicked()
    {
        // insert
        int row = ui->listView->currentIndex().row();
        model->insertRow(row);
    
        QModelIndex index = model->index(row);
        ui->listView->setCurrentIndex(index);
        ui->listView->edit(index);
    }
    
    void Dialog::on_pushButton_3_clicked()
    {
        // delete
        model->removeRows(ui->listView->currentIndex().row(),1);
    }

---
+++ ModelViewTest/dialog.cpp
@@ -0,0 +1,58 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+    model = new QStringListModel(this);
+
+    QStringList List;
+    List << "Cat" << "Dog" << "Rabbit";
+
+    model->setStringList(List);
+
+    ui->listView->setModel(model);
+    ui->comboBox->setModel(model);
+
+    ui->listView->setEditTriggers(QAbstractItemView::AnyKeyPressed | QAbstractItemView::DoubleClicked);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_pushButton_clicked()
+{
+    // add
+    int row = model->rowCount();
+    model->insertRow(row);
+
+
+    QModelIndex  index = model->index(row);
+
+    ui->listView->setCurrentIndex(index);
+    ui->listView->edit(index);
+}
+
+
+void Dialog::on_pushButton_2_clicked()
+{
+    // insert
+    int row = ui->listView->currentIndex().row();
+    model->insertRow(row);
+
+    QModelIndex index = model->index(row);
+    ui->listView->setCurrentIndex(index);
+    ui->listView->edit(index);
+}
+
+
+void Dialog::on_pushButton_3_clicked()
+{
+    // delete
+    model->removeRows(ui->listView->currentIndex().row(),1);
+}
+
---
+++ ModelViewTest/dialog.h
@@ -0,0 +1,34 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+#include <QtCore>
+#include <QtGui>
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Dialog;
+}
+QT_END_NAMESPACE
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Dialog(QWidget *parent = nullptr);
+    ~Dialog();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+private:
+    Ui::Dialog *ui;
+    QStringListModel *model;
+
+
+};
+#endif // DIALOG_H
